ea2ffed4d264ad54fd40254621e4739ad22bbe73
Klagefurt <klagefurt@gmail.com>
2026-06-06 22:34:25 +0300

    feat: add pipe for auth logic

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,13 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { TokenResponse } from './auth.interface';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
   http = inject(HttpClient);
+  cookieService = inject(CookieService);
+
+  accessToken: string | null = null;
+  refreshToken: string | null = null;
+
+  get isAuthenticated() {
+    if (!this.accessToken) {
+      this.accessToken = this.cookieService.get('access_token') || null;
+    }
+    return !!this.accessToken;
+  }
 
   login(username: string, password: string) {
-    return this.http.post('/auth/token', { username, password });
+
+    const body = {
+      username: username,
+      password: password
+    }
+ 
+    return this.http.post<TokenResponse>(
+      '/auth/token', 
+      body
+    ).pipe(
+      tap(resp => {
+        this.accessToken = resp.access_token;
+        this.refreshToken = resp.refresh_token;
+
+        this.cookieService.set('access_token', this.accessToken!, { path: '/' });
+        this.cookieService.set('refresh_token', this.refreshToken!, { path: '/' });
+      })
+    );
   }
 }
--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Auth } from '../../auth/auth';
+import { delay, from, map, skip, take, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -10,22 +12,27 @@ import { Auth } from '../../auth/auth';
 })
 export class LoginPage {
   auth = inject(Auth);
+  router = inject(Router);
 
   form = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)]),
   });
 
+
   onSubmit() {
     console.log("Form value:", this.form.value);
 
-    this.auth.login(this.form.value.username!, this.form.value.password!).subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (err) => {
-        console.error(err);
-      }
+    this.auth.login(
+      this.form.value.username!, this.form.value.password!)
+      .subscribe({
+        next: (res) => {
+          this.router.navigate(['/']);
+          console.log(res);
+        },
+        error: (err) => {
+          console.error(err);
+        }
     });
   }
 }
